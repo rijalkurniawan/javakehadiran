709f913d34d28503c7477f15380d7630eed51f7e
Rijal Kurniawan <rijalkurniawan1994@gmail.com>
2023-11-15 22:19:49 +0700

    Commit!!

---
+++ src/Config/Koneksi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Config;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author user
+ */
+public class Koneksi {
+        private static Connection conn;
+        public static Connection getConnection () {
+        if (conn == null) {
+        try{
+        String url = "jdbc:mysql://localhost:3306/java_kehadiran";
+        String user = "root";
+        String pass = "";
+        DriverManager.registerDriver (new com.mysql.jdbc.Driver());
+        conn = (Connection) DriverManager.getConnection (url,user,pass);
+       
+        } catch (Exception e) {
+            Logger.getLogger(Koneksi.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    return conn;
+    } 
+}
